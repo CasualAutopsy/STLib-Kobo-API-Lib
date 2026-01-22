668b5db3845d1ba1fcb3fae87daa754f06f50afb
CasualAutopsy <Casual_Autopsy@outlook.com>
2026-01-22 13:26:52 -0500

    Info API: Add API key args + Remove redundant variables

--- api/info.js
+++ api/info.js
@@ -1,13 +1,15 @@
 /**
  * Returns the Jinja template stored in the GGUF model, if found
  * @param {String} host - Kobold base URL
+ * @param {String} apikey - API authorization key
  * @returns {Promise<obj>}
  */
-export async function koboPropsInfo(host){
+export async function koboPropsInfo(host, apikey = undefined){
     try {
         const response = await fetch(`${host}/props`, {
             method: "GET",
             headers: {
+                "Authorization": `Bearer ${apikey !== undefined ? apikey : "None"}`,
                 "accept": "application/json"
             }
         });
@@ -21,18 +23,17 @@ export async function koboPropsInfo(host){
 /**
  * Retrieve the common API identity provider
  * @param {String} host - Kobold base URL
+ * @param {String} apikey - API authorization key
  * @returns {Promise<any>}
  */
-export async function koboServiceInfo(host){
-    const url = `${host}/.well-known/serviceinfo`;
-    const header = {
-        "accept": "application/json"
-    };
-
+export async function koboServiceInfo(host, apikey = undefined){
     try {
-        const response = await fetch(url, {
+        const response = await fetch(`${host}/.well-known/serviceinfo`, {
             method: "GET",
-            headers: header
+            headers: {
+                "Authorization": `Bearer ${apikey !== undefined ? apikey : "None"}`,
+                "accept": "application/json"
+            }
         });
 
         return await response.json();
